7956804da43daca4812b5a73857470365f75898a
CHANWOO LEE <chw210601@gmail.com>
2024-02-04 23:32:46 +0900

    ✨ feat : 24.02.04 이찬우 : conclude

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+const microServiceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: process.env.MICROSERVICE_SERVDR_HOST || 'localhost',
+    port: process.env.MICROSERVICE_SERVDR_PORT || 5000,
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microServiceOptions,
+  );
+  await app.listen();
 }
 bootstrap();
